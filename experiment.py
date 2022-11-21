8ec27bddb2f882863896e91843ea2837b4a63ded
Peter Harrison <pmch2@cam.ac.uk>
2022-11-21 18:27:53 +0000

    Prototyping

--- experiment.py
+++ experiment.py
@@ -1,6 +1,7 @@
 # pylint: disable=unused-import,abstract-method,unused-argument
-
+import json
 import random
+from statistics import mean
 
 from flask import Markup
 
@@ -8,80 +9,74 @@ import psynet.experiment
 from psynet.consent import MainConsent, NoConsent
 from psynet.modular_page import PushButtonControl
 from psynet.page import InfoPage, SuccessfulEndPage, ModularPage
-from psynet.timeline import Timeline
+from psynet.timeline import Timeline, Module, CodeBlock
 from psynet.trial.static import StaticTrial, StaticNode, StaticTrialMaker
 from psynet.utils import get_logger
 
-from psynet.js_synth import JSSynth, Chord, StretchedTimbre
+from psynet.js_synth import JSSynth, Chord, InstrumentTimbre
 
 logger = get_logger()
 
+with open("chord_types.json") as file:
+    NODES = [
+        StaticNode(
+            definition={
+                "chord_type": chord_type,
+                "duration": 2.5,
+                "roving_radius": 2.5,
+                "timbres": ["piano"],
+            },
+        )
+        for chord_type in json.load(file)
+    ]
 
-nodes = [
-    StaticNode(
-        definition={
-            "timbre": timbre,
-            "presentation": presentation,
-            "repetition": repetition,
-        },
-        block=presentation,
-    )
-    for timbre in ["stretched"]
-    for presentation in ["dichotic", "diotic"]
-    for repetition in range(25)
-]
-
-timbres = {
-    "stretched": StretchedTimbre(
-        attack=0.2,
-        decay=0.1,
-        sustain_amp=0.8,
-        release=1.0
+TIMBRES = {
+    "piano": InstrumentTimbre(
+        type="piano",
     ),
 }
 
+VOCAL_RANGES = {
+    "Soprano": 69,
+    "Alto": 65,
+    "Tenor": 57,
+    "Bass": 52,
+}
 
-class ConsonanceTrial(StaticTrial):
+
+class VerticalProcessingTrial(StaticTrial):
     time_estimate = 5
 
     def finalize_definition(self, definition, experiment, participant):
-        definition["duration"] = 1.8  # The original duration in Marjieh et al. was 1.3 s
-        definition["lower_pitch"] = random.uniform(55, 65)
-        definition["pitch_interval"] = random.uniform(11, 13.5)
-        definition["upper_pitch"] = definition["lower_pitch"] + definition["pitch_interval"]
+        mean_target_pitch = random.uniform(
+            participant.var.vocal_centre - self.definition["roving_radius"],
+            participant.var.vocal_centre + self.definition["roving_radius"]
+        )
 
-        if definition["presentation"] == "diotic":
-            definition["lower_pan"] = 0
-            definition["upper_pan"] = 0
-        else:
-            if random.choice([False, True]):
-                definition["lower_pan"] = -1
-                definition["upper_pan"] = 1
-            else:
-                definition["lower_pan"] = 1
-                definition["upper_pan"] = -1
+        mean_original_pitch = mean(definition["chord_type"])
+        pitch_translation = mean_target_pitch - mean_original_pitch
+        target_pitches = [p + pitch_translation for p in self.definition["chord_type"]]
+
+        definition["mean_target_pitch"] = mean_target_pitch
+        definition["target_pitches"] = target_pitches
 
         return definition
 
     def show_trial(self, experiment, participant):
+        timbre = [TIMBRES[timbre] for timbre in self.definition["timbres"]]
+
         return ModularPage(
-            "chord_player",
+            "vertical_processing_page",
             JSSynth(
                 Markup("Please rate the sound for <strong>pleasantness</strong> on a scale from 1 to 7."),
                 [
                     Chord(
-                        [
-                            self.definition["lower_pitch"],
-                            self.definition["upper_pitch"],
-                        ],
+                        self.definition["target_pitches"],
                         duration=self.definition["duration"],
-                        pan=[
-                            self.definition["lower_pan"],
-                            self.definition["upper_pan"],
-                        ],
+                        timbre=timbre,
                     )
                 ],
-                timbre=timbres[self.definition["timbre"]],
+                timbre=timbre,
             ),
             PushButtonControl(
                 choices=[1, 2, 3, 4, 5, 6, 7],
@@ -98,9 +93,32 @@ class ConsonanceTrial(StaticTrial):
             )
         )
 
+def get_voice_type():
+    return Module(
+        "get_voice_type",
+        ModularPage(
+            "get_voice_type",
+            "What voice type best describes you?",
+            PushButtonControl(
+                [
+                    "Soprano",
+                    "Alto",
+                    "Tenor",
+                    "Bass",
+                ]
+            ),
+            time_estimate=5,
+            save_answer="voice_type"
+        ),
+        CodeBlock(lambda participant: participant.var.set(
+                "vocal_centre",
+                VOCAL_RANGES[participant.var.voice_type],
+            )
+        )
+    )
 
 class Exp(psynet.experiment.Experiment):
-    label = "Dichotic stretching experiment"
+    label = "Vertical processing experiment"
 
     timeline = Timeline(
         NoConsent(),
@@ -108,12 +126,13 @@ class Exp(psynet.experiment.Experiment):
             "Welcome to the experiment!",
             time_estimate=5,
         ),
+        get_voice_type(),
         StaticTrialMaker(
             id_="consonance_main_experiment",
-            trial_class=ConsonanceTrial,
-            nodes=nodes,
-            expected_trials_per_participant=len(nodes),
-            max_trials_per_participant=len(nodes),
+            trial_class=VerticalProcessingTrial,
+            nodes=NODES,
+            expected_trials_per_participant=len(NODES),
+            max_trials_per_participant=len(NODES),
             recruit_mode="n_participants",
             allow_repeated_nodes=False,
             n_repeat_trials=0,
