4ffba8d0b7b44bcc5c28aaf6966b81efdccaa1d2
Peter Harrison <pmch2@cam.ac.uk>
2022-11-23 22:01:11 +0000

    Add terminal.sh

---
+++ terminal.sh
@@ -0,0 +1,20 @@
+set -euo pipefail
+
+. params.sh
+. services.sh
+. build.sh
+
+docker run \
+  --name dallinger \
+  --rm \
+  -ti \
+  -u $(id -u "${USER}"):$(id -g "${USER}") \
+  -v "${PWD}":/experiment \
+  -v "${HOME}"/.dallingerconfig:/.dallingerconfig \
+  --network dallinger \
+  -p 5000:5000 \
+  -e FLASK_OPTIONS='-h 0.0.0.0' \
+  -e REDIS_URL=redis://dallinger_redis:6379 \
+  -e DATABASE_URL=postgresql://dallinger:dallinger@dallinger_postgres/dallinger \
+  "${EXPERIMENT_IMAGE}" \
+  /bin/bash
